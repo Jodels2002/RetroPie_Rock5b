f32e755371258884757d7c2c99dd5fa3fb461f0e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-15 15:11:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,7 +25,8 @@ sudo systemctl daemon-reload
   sudo apt install -y csound-u*
   sudo apt install -y ntfs-3g 
   sudo apt install autofs -y
-  sudo apt install autoconf libtool intltool  -y
+  sudo apt install -y autoconf libtool intltool 
+  sudo apt install -y build-essential libgtk-3-dev
   sudo apt install raspi-config -y
   sudo raspi-config nonint do_expand_rootfs
   
