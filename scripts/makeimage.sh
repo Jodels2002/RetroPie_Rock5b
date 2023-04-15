f77b8c9d87d3ac62688dd440354098fe53a3d4aa
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-15 15:41:07 +0200

    Update makeimage.sh

--- scripts/makeimage.sh
+++ scripts/makeimage.sh
@@ -2,7 +2,7 @@
 sudo fdisk -l
 sudo umount /dev/sda1
 sudo dd if=/dev/nvme0n1 of=/dev/sda bs=4M status=progress
-sudo dd if=/dev/nvme0n1 of=Rockpi.img bs=4M status=progress
+sudo dd if=/dev/nvme0n1 of=/dev/sda1/RockPi.img bs=4M status=progress
 
 sudo losetup -fP /dev/sda/Rockpi.img
 sudo resize2fs /dev/loop1 5G
