157146b98a75ca98ef323990418b6401eeb0df5c
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-16 06:43:35 +0200

    Update makeimage.sh

--- scripts/makeimage.sh
+++ scripts/makeimage.sh
@@ -1,13 +1,18 @@
-# https://florianmuller.com/quickly-resize-and-shrink-raspberry-pi-sd-card-image-on-macos
+#!/bin/bash
+# B.Titze 2023      
+      BLACK='\033[0;39m'
+      BLUE='\033[1;34m'
+      GREEN='\033[1;32m'
+      RED='\033[1;31m'
+      GREY='\033[1;30m'# https://florianmuller.com/quickly-resize-and-shrink-raspberry-pi-sd-card-image-on-macos
 
 sudo fdisk -l
 sudo umount /dev/sda1
 sudo mkdir /media/usb
 sudo chmod -R 777 /media/usb
 sudo mount /dev/sda1 /media/usb
-
-
 sudo dd if=/dev/nvme0n1 of=/media/usb/RockPi.img bs=4M status=progress
+
 #sudo dd if=/dev/nvme0n1 of=/mnt bs=128K conv=noerror,sync
 
 #sudo losetup -fP /dev/sda/Rockpi.img
