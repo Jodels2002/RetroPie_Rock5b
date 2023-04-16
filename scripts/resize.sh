dfe2c58b729aae652be7418d43e6a5a3643850b0
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-16 09:32:51 +0200

    Update resize.sh

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -1,1 +1,13 @@
+
+
+echo "deb http://archive.raspberrypi.org/debian/ buster main" >> /etc/apt/sources.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 7FA3303E
+sudo apt install raspi-config
 sudo /etc/init.d/resize2fs start
+
+sudo wget https://raw.githubusercontent.com/canonical/cloud-utils/main/bin/growpart -P /usr/local/bin/
+sudo chmod +x /usr/local/bin/growpart
+
+# assuming we are resizing an ext4 partition "/dev/mmcblk1p1":
+sudo growpart /dev/mmcblk1 1
+sudo resize2fs /dev/mmcblk1p1
