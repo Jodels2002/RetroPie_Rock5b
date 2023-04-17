24fc001c3305f70b0d616c3db73a74d1c2b6d7f4
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-17 11:47:35 +0200

    Create Amiga.sh

---
+++ scripts/Amiga.sh
@@ -0,0 +1,57 @@
+#************************************************  Amiga        ************************************** 
+cd
+	     sudo rm -rf /opt/Amiga
+	     sudo mkdir /opt/Amiga
+	     sudo ln -s /opt/Amiga/ Amiga
+	     cd /opt/
+             unzip -u  /opt/RetroPie_Rock5b/Amiga/Amiga.zip
+       clear
+       toilet „Amiga“ --metal
+      echo " "
+      echo " "
+      echo "  First installation "
+      echo " "
+      
+       sudo rm -rf /opt/Amiga/conf/amiberry.conf
+       sudo rm -rf /opt/Amiga/conf/amiberry-osx.conf
+      
+      sudo apt install worker unadf xdms fonts-amiga -y
+      sudo apt install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4 libserialport0
+	clear
+      
+        toilet „Amiga“ --metal
+      echo " "
+      echo " "
+      echo "  First installation "
+      echo " "
+      cd 
+      unzip -u  /opt/RetroPie_Rock5b/config/worker.zip
+      
+      sudo cp -rf /opt/Amiga/data/AmigaTopaz.ttf /usr/share/fonts/truetype/amiga/
+      
+      sudo cp -R /opt/Amiga/data/amiberry.png /usr/share/applications/
+      sudo cp -R /opt/Amiga/data/amiberry_dev.png /usr/share/applications/
+      
+      
+            clear
+      toilet „Amiga“ --metal
+      echo " "
+      echo " "
+      echo "  First installation "
+      echo " "
+      
+      cd 
+      git clone --depth=1 https://github.com/rewtnull/amigafonts
+      sudo cp -rf /home/$USER/amigafonts/ttf/* /usr/share/fonts/truetype/
+      sudo rm -rf /home/$USER/amigafonts/
+      sudo rm -rf /opt/Amiga/conf/am*
+      
+
+     
+      clear
+        toilet „Amiga“ --metal
+      echo " "
+      echo " "
+      echo "  First installation "
+      echo " "
+      sudo ln -s /opt/vc/lib/libbcm_host.so /usr/lib/aarch64-linux-gnu/libbcm_host.so.0
