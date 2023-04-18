dbe565315f1166e4cbffb0edb3ec4ce33ac36601
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-18 13:31:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -80,7 +80,11 @@ sudo chmod -R 775 /usr/share/plymouth/
 	 sudo rm -rf /usr/share/plymouth/themes/spinner/watermark.png
 	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/themes/spinner/watermark.png
 	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/ubuntu-logo.png
-  	sudo cp -rf /opt/RetroPie_Rock5b/config/plymouth/AmigaKickstart /usr/share/plymouth/themes/
+	 
+	  sudo rm -rf /usr/share/plymouth/themes/armbian/watermark.png
+	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/themes/armbian/watermark.png
+	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/ubuntu-logo.png
+  	
   	sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/spinner/spinner.plymouth 500
 	sudo update-initramfs -u
 	
