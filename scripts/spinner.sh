d8dccf768c209cf04a2ebd763d812b07f50fcb0d
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-18 18:51:59 +0200

    Update spinner.sh

--- scripts/spinner.sh
+++ scripts/spinner.sh
@@ -1,7 +1,7 @@
 	 sudo rm -rf /usr/share/plymouth/themes/spinner/watermark.png
 	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.png /usr/share/plymouth/themes/spinner/watermark.png
 	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.png /usr/share/plymouth/ubuntu-logo.png
-	 
+	 sudo cp -rf /opt/RetroPie_Rock5b/config/spinner.plymouth /usr/share/plymouth/themes/spinner/
 	 
   	
   	sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/spinner/spinner.plymouth 500
