0482cec42b87ecaa8f5b0399b7d4449b33e1cda5
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-18 11:42:01 +0200

    Update makeimage.sh

--- scripts/makeimage.sh
+++ scripts/makeimage.sh
@@ -6,6 +6,6 @@ sudo fdisk -l
 sudo umount /dev/sda1
 sudo mkdir /media/usb
 sudo chmod -R 777 /media/usb
-sudo mount /dev/sda1 /media/usb
+
 sudo dd if=/dev/nvme0n1 of=/dev/sda1/nvme0n1.img bs=4M status=progress
 #sudo dd if=/dev/mmcblk1 of=/dev/sda1/mmcblk1.img bs=4M status=progress
