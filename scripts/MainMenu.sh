b3ea95dc83d9c2cecd522bf53c1d1372fcbf7b68
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-18 07:26:04 +0200

    Create MainMenu.sh

---
+++ scripts/MainMenu.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Set terminal colors
+BG_GRAY='\033[48;5;240m'
+TXT_DARK_GRAY='\033[38;5;240m'
+RESET_COLOR='\033[0m'
+
+# Display menu using dialog with Font Awesome font
+dialog --backtitle "Debian Game Emulators" \
+       --title "Main Menu" \
+       --nocancel \
+       --menu "\nPlease select an option:" 10 40 3 \
+       --font /usr/share/fonts/truetype/fontawesome-webfont.ttf \
+       1 "\uf11b  Start Amiberry" \
+       2 "\uf11b  Start Emu" \
+       3 "\uf00d  Quit" \
+       2> /tmp/menu.tmp
+
+# Get user input from temporary file
+choice=$(cat /tmp/menu.tmp)
+
+# Execute selected option
+case $choice in
+  1)
+    ./amiberry.sh
+    ;;
+  2)
+    ./emu.sh
+    ;;
+  3)
+    exit 0
+    ;;
+  *)
+    printf "Invalid choice\n"
+    ;;
+esac
+
+
+
+
