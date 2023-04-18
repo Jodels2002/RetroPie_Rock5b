918c6b5038e6c7d0b9d0a0c6e08edb6480231864
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-18 15:25:53 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -78,12 +78,12 @@ sudo systemctl start fan-control
 
 sudo chmod -R 775 /usr/share/plymouth/
 	 sudo rm -rf /usr/share/plymouth/themes/spinner/watermark.png
-	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/themes/spinner/watermark.png
-	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/ubuntu-logo.png
+	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.png /usr/share/plymouth/themes/spinner/watermark.png
+	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.png /usr/share/plymouth/ubuntu-logo.png
 	 
 	  sudo rm -rf /usr/share/plymouth/themes/armbian/watermark.png
-	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/themes/armbian/watermark.png
-	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.bmp /usr/share/plymouth/ubuntu-logo.png
+	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.png /usr/share/plymouth/themes/armbian/watermark.png
+	 sudo cp -rf /opt/RetroPie_Rock5b/config/Retrorock.png /usr/share/plymouth/ubuntu-logo.png
   	
   	sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/spinner/spinner.plymouth 500
 	sudo update-initramfs -u
