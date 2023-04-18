5743e5a9030e0fd2b77b5e94a51c82c59c5c0a1f
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-18 11:51:03 +0200

    Update makeimage.sh

--- scripts/makeimage.sh
+++ scripts/makeimage.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # B.Titze 2023      
-# https://florianmuller.com/quickly-resize-and-shrink-raspberry-pi-sd-card-image-on-macos
+      BLACK='\033[0;39m'
+      BLUE='\033[1;34m'
+      GREEN='\033[1;32m'
+      RED='\033[1;31m'
+      GREY='\033[1;30m'# https://florianmuller.com/quickly-resize-and-shrink-raspberry-pi-sd-card-image-on-macos
 
 sudo fdisk -l
 sudo umount /dev/sda1
 sudo mkdir /media/usb
 sudo chmod -R 777 /media/usb
-
-sudo dd if=/dev/nvme0n1 of=/dev/sda1/nvme0n1.img bs=4M status=progress
-#sudo dd if=/dev/mmcblk1 of=/dev/sda1/mmcblk1.img bs=4M status=progress
+sudo mount /dev/sda1 /media/usb
+sudo dd if=/dev/nvme0n1 of=/media/usb/nvme0n1.img bs=4M status=progress
+#sudo dd if=/dev/mmcblk1 of=/media/usb/mmcblk1.img bs=4M status=progress
