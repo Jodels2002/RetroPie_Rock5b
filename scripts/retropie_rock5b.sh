b9a99e8eaab3bb400491288b3d6d841965243653
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-19 06:48:29 +0200

    Update retropie_rock5b.sh

--- scripts/retropie_rock5b.sh
+++ scripts/retropie_rock5b.sh
@@ -8,6 +8,7 @@
 power
 clear
 rm -rf /opt/RetroPie_Rock5b
+sudo rm -rf /etc/update-motd.d/*
 sudo cp -rf ~/RetroPie_Rock5b /opt/
 sudo cp -rf ~/RetroPie_Rock5b/config/rc.local /etc/
 cp -rf ~/RetroPie_Rock5b/config/.bashrc ~/
