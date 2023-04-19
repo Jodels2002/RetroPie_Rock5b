5a14c618738be94ab11ad4a4d054150bc40bbf76
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-19 11:00:10 +0200

    Delete playmouth.sh

--- scripts/playmouth.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-get install plymouth -y
-
-
-
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /lib/plymouth/themes/amiga-logo.plymouth 100
-sudo update-initramfs -u
