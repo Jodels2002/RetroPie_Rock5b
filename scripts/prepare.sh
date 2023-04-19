0cc8963fc785b8b7b6033d6758b937e4d9295360
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-19 08:11:06 +0200

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -61,4 +61,20 @@ fi
       sudo cp -rf /usr/share/themes/Amiga3.x_hidpi/* /usr/share/themes/Default/xfwm4/
       sudo cp -rf /opt/RetroPie_Rock5b/config/rpd-wallpaper/Commodore/* /usr/share/backgrounds/  
 
-    
+    cd
+toilet "RetroRock" --metal
+      echo -e "$BLUE RetroRock ROM Operating System and Libraries" 
+      echo -e "$GREY Version V2.0 2020-2021 RetroRock-OS "
+echo " No Rights Reserved.  "
+echo -e "$BLACK "
+echo " Type 'd' start Workbench "
+echo -e "$BLUE "
+echo " 1.>  "
+echo -e " 1.>  ( e ) Emulationstation                    "
+echo -e " 1.>  ( d ) Linux Desktop                       "
+echo -e " 1.>  ( r ) Retropie setup                      "
+echo " 1.>  ( m ) Config Menu                            " 
+echo " 1.>  ( u ) RetroRock update                       " 
+echo " 1.>  ( s ) Shutdown                               "  
+echo -e "$GREY "
+cd
