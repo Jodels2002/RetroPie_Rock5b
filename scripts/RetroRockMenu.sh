52e1a5a931369ef65aabcede9a5d016781ee89ca
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-04-26 15:15:50 +0200

    Update RetroRockMenu.sh

--- scripts/RetroRockMenu.sh
+++ scripts/RetroRockMenu.sh
@@ -129,8 +129,8 @@ case $CHOICE in
               #************************************************  Compile Amiberry         ************************************** 
             clear
            
-             sudo apt install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4 libserialport0
-	     sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev libraspberrypi-dev
+             sudo apt -y install libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4 libserialport0
+	     sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev libserialport-dev
           
 	    clear
             
