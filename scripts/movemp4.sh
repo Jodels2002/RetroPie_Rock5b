e339584ef2f226538a54ca241dca9f0ffd499f3e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-05-13 10:23:44 +0200

    Update movemp4.sh

--- scripts/movemp4.sh
+++ scripts/movemp4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Replace "source_dir" with the path to the directory containing the .m4 files
-source_dir="/home/RetroPie/"
+source_dir="/home/pi/RetroPie/"
 
 # Replace "destination_dir" with the path to the directory where you want to move the .m4 files
-destination_dir="/home/RetroPie_MP4/"
+destination_dir="/home/pi/RetroPie_MP4/"
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$destination_dir"
