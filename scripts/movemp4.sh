c193468b0cee106ab2b64dc21841d73a6d13f97e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2023-05-13 10:21:10 +0200

    Update movemp4.sh

--- scripts/movemp4.sh
+++ scripts/movemp4.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 # Replace "source_dir" with the path to the directory containing the .m4 files
-source_dir="~/RetroPie"
+source_dir="/home/RetroPie/"
 
 # Replace "destination_dir" with the path to the directory where you want to move the .m4 files
-destination_dir="~/RetroPie_MP4"
+destination_dir="/home/RetroPie_MP4/"
 
 # Create the destination directory if it doesn't exist
 mkdir -p "$destination_dir"
 
-# Move all .m4 files to the destination directory
-find "$source_dir" -name "*.mp4" -type f -exec mv {} "$destination_dir" \;
+# Move all .m4 files to the destination directory while preserving subdirectories
+find "$source_dir" -name "*.m4" -type f -exec sh -c '
+  for file do
+    dest="$destination_dir/${file#$source_dir/}"
+    mkdir -p "$(dirname "$dest")"
+    mv "$file" "$dest"
+  done' sh {} +
+
+
+
+
+
+
+
+
